a7580c97f25802058674d64efd2c316c58598dca
alexeyvalko <valko.alexey@gmail.com>
2021-11-25 00:43:51 +0300

    fix: fix styles

--- task-05/src/autoScaleText.js
+++ task-05/src/autoScaleText.js
@@ -2,7 +2,7 @@ const autoScaleText = (parentWidth, childWidth, actualFontSize) => {
   const pW = parseInt(parentWidth, 10);
   const chW = parseInt(childWidth, 10);
   const percentage = 100 / (pW / chW);
-  if (Math.round(percentage) >= 70 && actualFontSize > 14) {
+  if (Math.round(percentage) >= 70 && actualFontSize > 16) {
     return actualFontSize - 2;
   }
   return actualFontSize;
