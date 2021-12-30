f4c1aeb593f747c6d4d3907faeee3e5c639f490b
alexeyvalko <valko.alexey@gmail.com>
2021-12-30 23:48:24 +0300

    feat: implement drop paper

--- task-07/src/TrashCan.js
+++ task-07/src/TrashCan.js
@@ -9,6 +9,8 @@ class TrashCan {
 
     this.paper = document.createElement('div');
     this.paper.classList.add('paper');
+
+    this.trashStack = [];
   }
 
   drawTrashCan(state) {
@@ -16,8 +18,8 @@ class TrashCan {
     this.trashCan.classList.add('trashcan', state);
   }
 
-  addPapers() {
-    let amount = 5;
+  addPapers(number) {
+    let amount = number;
     const papers = [];
     while (amount !== 0) {
       const paper = document.createElement('div');
@@ -36,7 +38,9 @@ class TrashCan {
 
   addListener() {
     const canHandler = () => {
-      this.trashCan.classList.toggle('opened');
+      if (this.trashStack.length > 0) {
+        this.trashCan.classList.toggle('opened');
+      }
     };
 
     this.trashCan.onmouseover = canHandler;
@@ -44,38 +48,73 @@ class TrashCan {
     this.dragAndDrop();
   }
 
+  getBelowElement(el, event) {
+    const currentElement = el;
+    currentElement.hidden = true;
+    const elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+    currentElement.hidden = false;
+    return elemBelow;
+  }
+
   dragAndDrop() {
     const downHandler = (e) => {
-      const paper = e.target;
-      console.dir(this.appContainer)
-      console.log(this.appContainer.getBoundingClientRect())
-      if (paper.matches('.paper')) {
-        console.dir(this.element)
-        const shiftX = e.clientX - paper.getBoundingClientRect().left + this.element.offsetLeft;
-        const shiftY = e.clientY - paper.getBoundingClientRect().top + this.element.offsetTop;
-        paper.ondragstart = false;
-        paper.style.zIndex = 1000;
+      let { target } = e;
+      let shiftX =
+        e.clientX -
+        target.getBoundingClientRect().left +
+        this.element.offsetLeft;
+      let shiftY =
+        e.clientY - target.getBoundingClientRect().top + this.element.offsetTop;
 
-        const moveAt = (pageX, pageY) => {
-          paper.style.left = `${pageX - shiftX}px`;
-          paper.style.top = `${pageY - shiftY}px`;
-        };
+      const moveAt = (pageX, pageY) => {
+        target.style.left = `${pageX - shiftX}px`;
+        target.style.top = `${pageY - shiftY}px`;
+      };
+      const moveHandler = (event) => {
+        moveAt(event.pageX, event.pageY);
+        const elemBelow = this.getBelowElement(target, event);
+        if (elemBelow.matches('.trashcan')) {
+          this.trashCan.classList.add('opened');
+        } else {
+          this.trashCan.classList.remove('opened');
+        }
+      };
 
+      if (target.matches('.paper')) {
+        console.dir(this.element);
+        target.ondragstart = false;
+        target.style.zIndex = 1000;
         moveAt(e.pageX, e.pageY);
+        document.onmousemove = moveHandler;
+      }
 
-        const moveHandler = (event) => {
-          moveAt(event.pageX, event.pageY);
-        };
-
+      if (target.matches('.trashcan') && this.trashStack.length > 0) {
+        this.trashStack.pop();
+        const paper = document.createElement('div');
+        paper.classList.add('paper');
+        this.element.append(paper);
+        target = paper;
+        target.style.left = `${e.pageX - target.offsetWidth / 2}px`;
+        target.style.top = `${e.pageY - target.offsetHeight / 2}px`;
+        shiftX =  e.clientX - target.getBoundingClientRect().left +
+        this.element.offsetLeft;shiftY =
+        e.clientY - target.getBoundingClientRect().top + this.element.offsetTop;
+        moveAt(e.pageX, e.pageY);
         document.onmousemove = moveHandler;
       }
     };
 
     const upHandler = (e) => {
-      const { target } = e;
-      if (target.matches('.paper')) {
-        target.style.zIndex = 1;
+      const paper = e.target;
+      const belowElement = this.getBelowElement(paper, e);
+      if (paper.matches('.paper')) {
+        paper.style.zIndex = 1;
         document.onmousemove = false;
+        if (belowElement.matches('.trashcan')) {
+          this.trashStack.push(true);
+          paper.remove();
+          this.trashCan.classList.remove('opened');
+        }
       }
     };
 
@@ -85,7 +124,7 @@ class TrashCan {
 
   render() {
     this.element.innerHTML = '';
-    this.addPapers();
+    this.addPapers(5);
     this.drawTrashCan('closed');
     this.appContainer.append(this.element);
     this.addListener();
