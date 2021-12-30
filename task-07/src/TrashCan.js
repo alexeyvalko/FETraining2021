098f19df52db5ac6bf6e0b71cf6247ad2e8ab574
alexeyvalko <valko.alexey@gmail.com>
2021-12-30 21:10:25 +0300

    feat: implement drag and drop

--- task-07/src/TrashCan.js
+++ task-07/src/TrashCan.js
@@ -22,12 +22,13 @@ class TrashCan {
     while (amount !== 0) {
       const paper = document.createElement('div');
       const topPosition = 120 + Math.round(Math.random() * 300);
-      const leftPosition = 220 + Math.round(Math.random() * 300 + Math.random() * 200);
+      const leftPosition =
+        220 + Math.round(Math.random() * 300 + Math.random() * 200);
       paper.classList.add('paper');
       paper.style.top = `${topPosition}px`;
       paper.style.left = `${leftPosition}px`;
       papers.push(paper);
-      amount -=1;
+      amount -= 1;
     }
 
     this.element.append(...papers);
@@ -40,6 +41,46 @@ class TrashCan {
 
     this.trashCan.onmouseover = canHandler;
     this.trashCan.onmouseleave = canHandler;
+    this.dragAndDrop();
+  }
+
+  dragAndDrop() {
+    const downHandler = (e) => {
+      const paper = e.target;
+      console.dir(this.appContainer)
+      console.log(this.appContainer.getBoundingClientRect())
+      if (paper.matches('.paper')) {
+        console.dir(this.element)
+        const shiftX = e.clientX - paper.getBoundingClientRect().left + this.element.offsetLeft;
+        const shiftY = e.clientY - paper.getBoundingClientRect().top + this.element.offsetTop;
+        paper.ondragstart = false;
+        paper.style.zIndex = 1000;
+
+        const moveAt = (pageX, pageY) => {
+          paper.style.left = `${pageX - shiftX}px`;
+          paper.style.top = `${pageY - shiftY}px`;
+        };
+
+        moveAt(e.pageX, e.pageY);
+
+        const moveHandler = (event) => {
+          moveAt(event.pageX, event.pageY);
+        };
+
+        document.onmousemove = moveHandler;
+      }
+    };
+
+    const upHandler = (e) => {
+      const { target } = e;
+      if (target.matches('.paper')) {
+        target.style.zIndex = 1;
+        document.onmousemove = false;
+      }
+    };
+
+    this.appContainer.onmousedown = downHandler;
+    this.appContainer.onmouseup = upHandler;
   }
 
   render() {
