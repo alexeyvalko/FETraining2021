6cccac5e64a249c3003cb4adc0ce84df28406904
alexeyvalko <valko.alexey@gmail.com>
2022-01-01 21:32:14 +0300

    feat: add event class

---
+++ task-08/src/Event.js
@@ -0,0 +1,11 @@
+class Event {
+  constructor({name, date, time, description, priority}) {
+    this.name = name
+    this.date = date
+    this.time = time
+    this.description = description
+    this.priority = priority
+  }
+}
+
+export default Event;
--- task-08/src/app.js
+++ task-08/src/app.js
@@ -1,13 +1,24 @@
-
 class App {
   constructor(rootElement) {
     this.rootElement = rootElement;
     this.appContainer = document.createElement('div');
+    this.appContainer.classList.add('app-container');
   }
 
-
+  generateItems(n) {
+    let amount = n;
+    const items = [];
+    while (amount) {
+      const item = document.createElement('div');
+      item.classList.add('app-item');
+      items.push(item);
+      amount -= 1;
+    }
+    return items;
+  }
 
   render() {
+    this.appContainer.append(...this.generateItems(5));
     this.rootElement.append(this.appContainer);
   }
 }
