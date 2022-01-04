0ecce501a7e710573d3c42a5b397753128ff74dc
alexeyvalko <valko.alexey@gmail.com>
2022-01-04 15:45:47 +0300

    init: init task

---
+++ task-09/src/app.js
@@ -0,0 +1,13 @@
+
+class App {
+  constructor(rootElement) {
+    this.rootElement = rootElement;
+    this.appContainer = document.createElement('div');
+  }
+
+   render() {
+    this.rootElement.append(this.appContainer);
+  }
+}
+
+export default App;
